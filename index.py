55f9f2f7d21431bd9b5d1bf96e9159f01e106ccd
gbrown9 <gabe.brown@myemail.indwes.edu>
2015-11-09 18:44:27 +0000

    Week Nine Anagrams

---
+++ index.py
@@ -0,0 +1,27 @@
+#CIS 125
+
+#Gabe Brown and Daniel McMurray
+
+
+
+sortedWords = ""
+myWords = []
+
+
+
+quiz = open("quizwords.txt", "r")
+wordList = open("wordLists.txt", "r")
+
+myQuiz = quiz.read()
+quiz = quiz.replace(",", "")
+myWords = myQuiz.split()
+myWords = list(myWords)
+
+def sortedWord(myWords):
+    
+
+def main()
+
+
+quiz.close()
+wordList.close()
---
+++ newIndex.py
@@ -0,0 +1,46 @@
+#IWU - CIS - 125
+
+#Gabe Brown
+
+
+sortLetters = ""
+word = ""
+import string
+
+def sortLetter(word):
+    word = word.lower()
+    listWord = list(word)
+    listWord.sort()
+    sortedLetters = ''.join(listWord)
+    
+    return sortedLetters
+
+def createDictionary(fileName, dictionary):
+    fileHandle = open(fileName, 'r')
+    for line in fileHandle:
+        line = line.strip()
+        line = line.strip(string.punctuation)
+        word = line.lower()
+        if word[0] == 'v':
+            sortedWord = sortLetter(word)
+            dictionary[sortedWord] == word
+    fileHandle.close()    
+
+def findAnagrams(fileName, dictionary):
+    fileHandle = open(fileName, 'r')
+    for line in fileHandle:
+        line = line.strip()
+        quizword = line.lower()
+        print(quizword, dictionary[sortLetter(quizword)])
+    fileHandle.close()  
+
+def printAnagrams(fileName, dictionary):
+    pass
+
+
+def main():
+    aDict = {}
+    fileName = 'wordList.txt'
+    quizListName = 'quizword.txt'
+    createDictionary(fileName, aDict)
+    findAnagrams(quizlistName, aDict)
